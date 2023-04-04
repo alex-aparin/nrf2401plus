d24a59d5173fc084ccbca812be54d05ce712d416
AlexAkel <cool.ipatovo@gmail.com>
2023-04-04 07:33:38 +0300

    Registers map filling was made

--- include/nrf2401p.h
+++ include/nrf2401p.h
@@ -1,21 +1,21 @@
 #ifndef NRF2401P_H_INCLUDED__
 #define NRF2401P_H_INCLUDED__
 
-enum Nrf_DataRate
+typedef enum 
 {
 	NRF_1MBPS,
 	NRF_2MPBS,
 	NRF_250KBPS
-};
+} Nrf_DataRate;
 
-enum Nrf_CNC
+typedef enum 
 {
 	NRF_CNC_NONE = 0x0,
 	NRF_CNC_1BYTE,
 	NRF_CNC_2BYTE
-};
+} Nrf_CNC;
 
-enum Nrf_DataPipeId
+typedef enum 
 {
 	NRF_DATA_PIPE_0 = 0x1,
 	NRF_DATA_PIPE_1 = 0x2,
@@ -23,51 +23,60 @@ enum Nrf_DataPipeId
 	NRF_DATA_PIPE_3 = 0x4,
 	NRF_DATA_PIPE_4 = 0x5,
 	NRF_DATA_PIPE_5 = 0x6
-}
+} Nrf_DataPipeId;
 
-enum Nrf_AddressWidth
+typedef enum 
 {
 	NRF_ADDR_3BYTES = 0x1,
 	NRF_ADDR_4BYTES = 0x2,
 	NRF_ADDR_5BYTES = 0x3
-}
+} Nrf_AddressWidth;
 
-enum Nrf_TransmitPower
+typedef enum 
 {
 	NRF_OUT_POWER_18DBM = 0x0,
 	NRF_OUT_POWER_12DBM = 0x1,
 	NRF_OUT_POWER_6DBM = 0x2,
 	NRF_OUT_POWER_0DBM = 0x3
-}
+} Nrf_TransmitPower;
 
-struct Nrf_DataPipeOptions
+typedef struct 
 {
 	uint8_t auto_ack;
 	uint8_t addr_postfix;
 	uint8_t payload_size;
-};
+} Nrf_DataPipeOptions;
 
-struct Nrf_ReceiveOptions
+typedef struct 
 {
-	uint8_t[4] addr_prefix;
-	Nrf_DataPipeOptions[6] data_pipes;
+	uint8_t addr_prefix[4];
+	Nrf_DataPipeOptions data_pipes[6];
 	Nrf_DataPipeId pipes_count;
 	Nrf_AddressWidth address_width;
-};
+} Nrf_ReceiveOptions;
 
-struct Nrf_TransmitOptions
+typedef struct 
 {
 	Nrf_TransmitPower power;
-};
+} Nrf_TransmitOptions;
 
-struct Nrf_GlobalOptions
+typedef enum 
 {
+	NRF_INTERRUPT_NONE = 0x0,
+	NRF_INTERRUPT_RX = 0x1,
+	NRF_INTERRUPT_TX = 0x2,
+	NRF_INTERRUPT_MAX_RT = 0x4
+} Nrf_InterruptMask;
+
+typedef struct 
+{
+	Nrf_InterruptMask interrupt_mask;
 	uint8_t rf_channel;	//	up to 128 
 	Nrf_DataRate data_rate;
 	Nrf_CNC cnc;
 	Nrf_ReceiveOptions* receive_settings;
 	Nrf_TransmitOptions* transmit_options;
-};
+} Nrf_GlobalOptions;
 
 void Nrf_WriteSpi(uint8_t* data, uint8_t len);
 void Nrf_ReadSpi(uint8_t* data, uint8_t len);
--- source/nrf2401p.c
+++ source/nrf2401p.c
@@ -1,5 +1,7 @@
 #include "nrf2401p.h"
 
+#define TO_INT(x) ((x) ? 1 : 0)
+
 //	Register map table
 #define REG_CONFIG 		0x0 	//	Configuration register
 #define REG_ENAA 		0x1		//	Auto acknowledgement for data pipes
@@ -28,11 +30,107 @@
 #define	REG_DYNPD		0x1C	//	Enable dynamic payload length
 #define REG_FEATURE		0x1D	//	Feature register
 
+//	Register mnemonics
+#define REG_CONFIG_MASK_RX_DR  6
+#define REG_CONFIG_MASK_TX_DS  5
+#define REG_CONFIG_MASK_MAX_RT 4
+#define REG_CONFIG_EN_CRC 	   3
+#define REG_CONFIG_CRCO 	   2
+#define REG_CONFIG_PWR_UP 	   1
+#define REG_CONFIG_PRIM_RX 	   0
+
+#define REG_RF_SETUP_RF_DR_LOW 5
+#define REG_RF_SETUP_RF_DR_HIGH 3
+
+#define REG_FEATURE_EN_DPL 2
+
+//	NRF2401+ commands
+static void readRegister(uint8_t reg, uint8_t* const data)
+{
+}
+
+static void writeRegister(uint8_t reg, const uint8_t* const data, uint8_t len)
+{
+}
+
+static void readPayload(uint8_t* const data)
+{
+}
+
+static void writePayload(const uint8_t* const data, uint8_t len)
+{
+}
+
+static void flushTxFifo()
+{
+}
+
+static void flushRxFifo()
+{
+}
+
 
 //	Global functions implementation
 void Nrf_Init(const Nrf_GlobalOptions* const options)
 {
-	if (options == NULL)
+	if (!options)
 		return;
-	
+	uint8_t registers[REG_FEATURE+1] = {0};
+	if (options->receive_settings)
+	{
+		const Nrf_ReceiveOptions* const receive_options = &options->receive_settings;
+		const int pipes_count = receive_options->pipes_count;
+		for (int i = 0; i < pipes_count; ++i)
+		{
+			registers[REG_EN_RXADDR] |= 1 << i;
+			registers[REG_ENAA] |= TO_INT(receive_options->data_pipes[i].auto_ack) << i;
+		}
+		registers[REG_SETUP_AW] = receive_options->address_width;
+		int dynamic_payload = 0;
+		for (int i = 0; i < pipes_count; ++i)
+		{
+			if (receive_options->data_pipes[i].payload_size < 32)
+			{
+				dynamic_payload = 1;
+				registers[REG_RX_PW_P0 +i] = receive_options->data_pipes[i].payload_size;
+				registers[REG_DYNPD] |= 1 << i;
+			}
+			else
+			{
+				registers[REG_RX_PW_P0 +i] = 32;
+			}
+			if (i == 0 || i == 1)
+			{
+				uint8_t addr[5];
+				addr[0] = receive_options->data_pipes[i].addr_postfix;
+				addr[1] = receive_options->addr_prefix[0];
+				addr[2] = receive_options->addr_prefix[1];
+				addr[3] = receive_options->addr_prefix[2];
+				addr[4] = receive_options->addr_prefix[3];
+				registers[REG_RX_ADDR_P0 + i] = 0;	//	TODO: (alex) use here sending
+				continue;
+			}
+			registers[REG_RX_ADDR_P0 + i] = receive_options->data_pipes[i].addr_postfix;	//	TODO: (alex) use here sending
+		}
+		registers[REG_FEATURE] |= TO_INT(dynamic_payload) << REG_FEATURE_EN_DPL;
+	}
+	{
+		registers[REG_CONFIG] |= TO_INT((options->interrupt_mask & NRF_INTERRUPT_RX) != NRF_INTERRUPT_RX) << REG_CONFIG_MASK_RX_DR;
+		registers[REG_CONFIG] |= TO_INT((options->interrupt_mask & NRF_INTERRUPT_TX) != NRF_INTERRUPT_TX) << REG_CONFIG_MASK_TX_DS;
+		registers[REG_CONFIG] |= TO_INT((options->interrupt_mask & NRF_INTERRUPT_MAX_RT) != NRF_INTERRUPT_MAX_RT) << REG_CONFIG_MASK_MAX_RT;
+		registers[REG_CONFIG] |= TO_INT(options->cnc != NRF_CNC_NONE || registers[REG_ENAA] != 0x0) << REG_CONFIG_EN_CRC;
+		registers[REG_CONFIG] |= TO_INT(options->cnc == NRF_CNC_2BYTE) << REG_CONFIG_CRCO;
+		registers[REG_CONFIG] |= 1 << REG_CONFIG_PWR_UP;
+	}
+	registers[REG_SETUP_RETR] = 0x3; // Recet value
+	registers[REG_RF_CH] = options->rf_channel & 0x7f;
+	if (options->data_rate == NRF_250KBPS)
+	{
+		registers[REG_RF_SETUP] |= 1 << REG_RF_SETUP_RF_DR_LOW;
+	}
+	else
+	{
+		registers[REG_RF_SETUP] |= TO_INT(options->data_rate == NRF_2MPBS) << REG_RF_SETUP_RF_DR_LOW;
+	}
+	registers[REG_RF_SETUP] |= options->transmit_options ? options->transmit_options.power : 0x3;
 }
